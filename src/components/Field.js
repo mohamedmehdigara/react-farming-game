b17039e31206ad0341b211d44b15d36e207fba68
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-31 17:09:11 +0100

    added files

--- src/components/Field.js
+++ src/components/Field.js
@@ -7,12 +7,18 @@ const plants = [];
 const Field = styled.div`
   width: 100px;
   height: 100px;
-  border: 1px solid black;
+  border: 1px solid #ccc;
   background-color: #fff;
   cursor: pointer;
+  border-radius: 5px;
 
   &.is-planted {
     background-color: #00ff00;
+  }
+
+  &.is-hovered {
+    background-color: #eee;
+    box-shadow: 0 0 3px #ccc;
   }
 `;
 
@@ -32,17 +38,20 @@ const MyField = ({ fieldIndex, onClick }) => {
   // Get the plant that is planted in the field.
   const plant = getPlant(fieldIndex);
 
-  // Determine the CSS class of the field based on whether or not there is a plant planted in it.
+  // Determine the CSS class of the field based on whether or not there is a plant planted in it and whether it is hovered over.
   const isPlanted = !!plant;
+  const isHovered = false; // TODO: Implement this
 
   // Return the field.
   return (
     <Field
-      className={isPlanted ? "is-planted" : ""}
-      onClick={onClick}
-    >
-      {plant && plant.stage}
-    </Field>
+  fieldIndex={0}
+  onClick={() => alert('You clicked on field 0!')}
+>
+  <h1>Field</h1>
+  <p>Click to plant a seed.</p>
+</Field>
+
   );
 };
 
