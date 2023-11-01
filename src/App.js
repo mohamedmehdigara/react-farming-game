e3ee962753079ddf34c9d7d20d47caf58355815b
mohamedmehdigara <garamehdi57@yahoo.com>
2023-11-01 13:13:39 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import SeedList from "./components/SeedList";
 import Field from "./components/Field";
 import Farm from "./components/Farm";
 import HarvestedDisplay from "./components/HarvestedDisplay";
+import PlantDialog from "./components/PlantDialog";
+import Leaderboard from "./components/Leaderboard";
 
 import "./App.css";
 
 const App = () => {
+  const [plantDialogOpen, setPlantDialogOpen] = useState(false);
+
+  const openPlantDialog = () => {
+    setPlantDialogOpen(true);
+  };
+
+  const closePlantDialog = () => {
+    setPlantDialogOpen(false);
+  };
+
   return (
     <div className="App">
       <h1>Farm Game</h1>
@@ -16,7 +28,11 @@ const App = () => {
         <Field />
         <Farm />
         <HarvestedDisplay />
+        <Leaderboard />
+        <PlantDialog open={plantDialogOpen} onClose={closePlantDialog} />
       </FarmStateProvider>
+
+      <button onClick={openPlantDialog}>Plant a seed</button>
     </div>
   );
 };
