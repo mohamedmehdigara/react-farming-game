4b663f9f1a5a4832611ce29ca389e890735f0ea7
mohamedmehdigara <garamehdi57@yahoo.com>
2023-11-01 16:43:12 +0100

    edited files

--- src/components/Seed.js
+++ src/components/Seed.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import React from "react";
 
 const Seed = styled.div`
   width: 50px;
@@ -15,6 +16,9 @@ const Seed = styled.div`
     background-color: #ccc;
     cursor: default;
   }
+
+  /* Added a CSS transition to the background-color property. This will make the seed animation when it is selected or deselected. */
+  transition: background-color 0.2s ease-in-out;
 `;
 
 const MySeed = ({ name, stage, isSelected, isDisabled, onClick }) => {
