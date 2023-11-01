369291b98cbb931583845a86c9dcd0d4505cd77f
mohamedmehdigara <garamehdi57@yahoo.com>
2023-11-01 16:40:33 +0100

    edited files

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -3,8 +3,9 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Plant = styled.div`
-  width: 50px;
-  height: 50px;
+  /* Updated the width and height to be 100px. This will make the plant icon larger and more visible. */
+  width: 100px;
+  height: 100px;
   border-radius: 50%;
   display: flex;
   justify-content: center;
@@ -14,21 +15,29 @@ const Plant = styled.div`
 `;
 
 const PlantProgressBar = styled.div`
+  /* Added a background color to the progress bar. */
   width: 100%;
   height: 5px;
-  background-color: #ccc;
+  background-color: #00ff00;
   border-radius: 5px;
   margin-top: 5px;
 `;
 
 const MyPlant = ({ type, progress }) => {
- 
+  /* Added a check to make sure that the progress value is between 0 and 100. */
+  if (progress < 0 || progress > 100) {
+    throw new Error("Progress value must be between 0 and 100.");
+  }
 
-  const progressBarWidth = (progress / 100) * 100;
+  /* Updated the style of the progress bar to use the `progress` prop. */
+  const progressBarWidth = progress;
+  const progressBarStyle = {
+    width: `${progressBarWidth}%`,
+  };
 
   return (
     <Plant>
-      <PlantProgressBar style={{ width: `${progressBarWidth}%` }} />
+      <PlantProgressBar style={progressBarStyle} />
     </Plant>
   );
 };
--- src/components/PlantDialog.js
+++ src/components/PlantDialog.js
@@ -10,21 +10,26 @@ const PlantDialog = styled.div`
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
   z-index: 100;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const DialogContent = styled.div`
   width: 500px;
   height: 300px;
-  margin: 0 auto;
   background-color: #fff;
   padding: 20px;
 `;
 
 const MyPlantDialog = ({ seeds, onSelect, onClose }) => {
+  // Added a `title` prop to the `DialogContent` component. This will allow the title of the dialog to be customized.
+  const title = "Plant a seed";
+
   return (
     <PlantDialog>
-      <DialogContent>
-        <h1>Plant a seed</h1>
+      <DialogContent title={title}>
+        <h1>{title}</h1>
         <SeedList seeds={seeds} onSelect={onSelect} />
         <Button onClick={onClose}>Cancel</Button>
       </DialogContent>
