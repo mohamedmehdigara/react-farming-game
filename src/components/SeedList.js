5eb181bedbe8d7990aab0468e1ded4f9fb36af80
mohamedmehdigara <garamehdi57@yahoo.com>
2023-11-01 16:45:33 +0100

    edited files

--- src/components/SeedList.js
+++ src/components/SeedList.js
@@ -1,15 +1,19 @@
 import styled from "styled-components";
 import MySeed from "./Seed";
+import React from "react";
 
 const SeedList = styled.ul`
   width: 100px;
   height: 100px;
   border: 1px solid black;
   background-color: #fff;
+  list-style-type: none;
+  padding: 0;
 
   li {
     display: inline-block;
     margin: 5px;
+    cursor: pointer;
   }
 
   & {
@@ -17,14 +21,21 @@ const SeedList = styled.ul`
   }
 `;
 
-const MySeedList = ({ seeds }) => {
+const MySeedList = ({ seeds, onSelect }) => {
+  // Added a `onSelect` prop to the `MySeedList` component. This will allow the seed list to notify its parent component when a seed is selected.
+
   return (
-    <ul>
-      {seeds.map((seed, index) => (
-        <li key={index}>{seed.name}</li>
+    <SeedList>
+      {seeds && seeds.map((seed, index) => (
+        <MySeed
+          key={index}
+          name={seed.name}
+          isSelected={seed.isSelected}
+          onClick={() => onSelect(seed.id)}
+        />
       ))}
-    </ul>
+    </SeedList>
   );
 };
 
-export default SeedList;
+export default MySeedList;
