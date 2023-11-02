fa00f4ff16b60194894aad8d5a29e7068c4efc57
mohamedmehdigara <garamehdi57@yahoo.com>
2023-11-02 14:20:40 +0100

    edited files

--- src/components/HarvestedDisplay.js
+++ src/components/HarvestedDisplay.js
@@ -9,14 +9,21 @@ const HarvestedDisplay = () => {
   // Add a total harvested plants count.
   const totalHarvestedPlants = harvestedPlants.length;
 
+  // Add a check if there are any harvested plants. If not, display a message instead of an empty list.
+  const harvestedPlantsList = harvestedPlants.length > 0 ? (
+    <ul>
+      {harvestedPlants.map((plant) => (
+        <li key={plant.name}>{plant.name}</li>
+      ))}
+    </ul>
+  ) : (
+    <p>No plants have been harvested yet.</p>
+  );
+
   return (
     <div>
       <h2>Harvested Plants ({totalHarvestedPlants})</h2>
-      <ul>
-        {harvestedPlants.map((plant) => (
-          <li key={plant.name}>{plant.name}</li>
-        ))}
-      </ul>
+      {harvestedPlantsList}
     </div>
   );
 };
--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -3,30 +3,36 @@ import React, { useState, useEffect } from "react";
 const Leaderboard = () => {
   const [users, setUsers] = useState([]);
 
-  // Create a dummy leaderboard
-  const dummyLeaderboard = [
-    { name: "Alice", score: 100 },
-    { name: "Bob", score: 90 },
-    { name: "Carol", score: 80 },
-    { name: "Dave", score: 70 },
-    { name: "Eve", score: 60 },
-  ];
+  // Create a function to fetch the leaderboard data from an API.
+  const fetchLeaderboardData = async () => {
+    // TODO: Implement this function to fetch the leaderboard data from an API.
+    const response = await fetch("/leaderboard");
+    const data = await response.json();
+    setUsers(data);
+  };
 
-  // Set the leaderboard state
+  // Set the leaderboard state using the fetchLeaderboardData function.
   useEffect(() => {
-    setUsers(dummyLeaderboard);
+    fetchLeaderboardData();
   }, []);
+
+  // Add a check if the users array is empty. If so, display a loading message.
+  const usersList = users.length > 0 ? (
+    <ul>
+      {users.map((user, index) => (
+        <li key={index}>
+          {user.name}: {user.score}
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <p>Loading leaderboard data...</p>
+  );
 
   return (
     <div>
       <h1>Leaderboard</h1>
-      <ul>
-        {users.map((user, index) => (
-          <li key={index}>
-            {user.name}: {user.score}
-          </li>
-        ))}
-      </ul>
+      {usersList}
     </div>
   );
 };
