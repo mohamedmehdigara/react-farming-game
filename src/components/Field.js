35fbb41a74ceb8d81bb7d9baead7d53b61719792
mohamedmehdigara <mohamedmehdigara10@gmail.com>
2024-08-06 21:55:41 +0100

    edited files

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,25 +1,8 @@
-import styled from "styled-components";
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-const PlantIcon = () => {
-  return (
-    <svg viewBox="0 0 100 100">
-      <circle cx="50" cy="50" r="40" fill="#00ff00" />
-      <path fill="#fff" d="M50,25 L50,50 L20,50 L20,75 L50,75 Z" />
-    </svg>
-  );
-};
 
-const WaterIcon = () => {
-  return (
-    <svg viewBox="0 0 100 100">
-      <path fill="#000" d="M50,10 L50,90 L20,90 L20,70 L50,70 Z" />
-      <path fill="#fff" d="M50,70 L80,70 L80,90 L50,90 Z" />
-    </svg>
-  );
-};
-
-const Field = styled.div`
+const FieldContainer = styled.div`
   width: 100px;
   height: 100px;
   border: 1px solid #ccc;
@@ -27,6 +10,7 @@ const Field = styled.div`
   cursor: pointer;
   border-radius: 5px;
   transition: all 0.2s ease-in-out;
+  position: relative; // To position plant and water icons
 
   &.is-planted {
     background-color: #00ff00;
@@ -42,15 +26,7 @@ const Field = styled.div`
   }
 `;
 
-const getPlant = (fieldIndex) => {
-  const plants = []; // Your code for getting the plant at the given field index
-  return plants[fieldIndex] || null;
-};
-
-const MyField = ({ fieldIndex, onClick, onSelect = () => {}, title, ariaLabel, waterLevel }) => {
-  const plant = getPlant(fieldIndex);
-  const isPlanted = getPlant(fieldIndex) !== null;
-
+const Field = ({ isPlanted, onClick, onSelect, title, ariaLabel, waterLevel }) => {
   const [isHovered, setIsHovered] = useState(false);
   const [isFocused, setIsFocused] = useState(false);
 
@@ -58,15 +34,12 @@ const MyField = ({ fieldIndex, onClick, onSelect = () => {}, title, ariaLabel, w
     onSelect && onSelect();
   };
 
-  const plantIconDisplay = plant && <PlantIcon />;
-  const waterIconDisplay = waterLevel < 100 && <WaterIcon />;
-
   return (
-    <Field
+    <FieldContainer
       role="button"
       tabIndex={0}
       aria-label={ariaLabel || title}
-      className={`${isPlanted ? "is-planted" : ""} ${isHovered ? "is-hovered" : ""} ${isFocused ? "is-focused" : ""}`}
+      className={`${isPlanted ? 'is-planted' : ''} ${isHovered ? 'is-hovered' : ''} ${isFocused ? 'is-focused' : ''}`}
       onClick={onClick}
       onSelect={handleSelect}
       onMouseEnter={() => setIsHovered(true)}
@@ -74,10 +47,9 @@ const MyField = ({ fieldIndex, onClick, onSelect = () => {}, title, ariaLabel, w
       onFocus={() => setIsFocused(true)}
       onBlur={() => setIsFocused(false)}
     >
-      {plantIconDisplay}
-      {waterIconDisplay}
-    </Field>
+     
+    </FieldContainer>
   );
 };
 
-export default MyField;
+export default Field;
