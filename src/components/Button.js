28e9a5d9d937646c5fb5efa12ecd5f02c1d0f488
mohamedmehdigara <mohamedmehdigara10@gmail.com>
2024-08-06 21:49:15 +0100

    edited files

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,36 +1,38 @@
+import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const Button = styled.button`
+const ButtonContainer = styled.button`
   width: 100px;
   height: 40px;
   border-radius: 5px;
   border: none;
-
-  cursor: pointer;
+    cursor: pointer;
   font-weight: bold;
   transition: all 0.2s ease-in-out;
 
-  
+ 
+
   &:disabled {
     opacity: 0.5;
     cursor: not-allowed;
   }
 `;
 
-const MyButton = ({ children, onClick, disabled, variant }) => {
+const Button = ({ children, onClick, disabled, variant, isLoading }) => {
   return (
-    <Button onClick={onClick} disabled={disabled} variant={variant}>
-      {children}
-    </Button>
+    <ButtonContainer type="button" onClick={onClick} disabled={disabled || isLoading}>
+      {isLoading ? 'Loading...' : children}
+    </ButtonContainer>
   );
 };
 
-MyButton.propTypes = {
+Button.propTypes = {
   children: PropTypes.node.isRequired,
   onClick: PropTypes.func,
   disabled: PropTypes.bool,
-  variant: PropTypes.string,
+  variant: PropTypes.oneOf(['primary', 'secondary', 'danger']), // Add more variants as needed
+  isLoading: PropTypes.bool,
 };
 
-export default MyButton;
+export default Button;
