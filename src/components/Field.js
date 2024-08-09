78aa86c94fd2fef09cc852d93e2f85043bb5b00f
mohamedmehdigara <mohamedmehdigara10@gmail.com>
2024-08-09 16:47:42 +0100

    added files

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-
+import PlantIcon from './PlantIcon';
+import WaterIcon from './WaterIcon';
 
 const FieldContainer = styled.div`
-  width: 100px;
+  width: ${({ size }) => `${size * 100}px`};
   height: 100px;
   border: 1px solid #ccc;
   background-color: #fff;
   cursor: pointer;
   border-radius: 5px;
   transition: all 0.2s ease-in-out;
-  position: relative; // To position plant and water icons
+  position: relative;
 
   &.is-planted {
     background-color: #00ff00;
@@ -26,12 +27,12 @@ const FieldContainer = styled.div`
   }
 `;
 
-const Field = ({ isPlanted, onClick, onSelect, title, ariaLabel, waterLevel }) => {
+const Field = ({ fieldIndex, isPlanted, onClick, onSelect, title, ariaLabel, waterLevel, size }) => {
   const [isHovered, setIsHovered] = useState(false);
   const [isFocused, setIsFocused] = useState(false);
 
   const handleSelect = () => {
-    onSelect && onSelect();
+    onSelect && onSelect(fieldIndex); // Pass fieldIndex to onSelect handler
   };
 
   return (
@@ -46,8 +47,10 @@ const Field = ({ isPlanted, onClick, onSelect, title, ariaLabel, waterLevel }) =
       onMouseLeave={() => setIsHovered(false)}
       onFocus={() => setIsFocused(true)}
       onBlur={() => setIsFocused(false)}
+      size={size}
     >
-     
+      {isPlanted && <PlantIcon />}
+      {waterLevel < 100 && <WaterIcon />}
     </FieldContainer>
   );
 };
---
+++ src/components/PlantIcon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PlantIcon = () => {
+  return (
+    <svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+      <path d="M50 10 L50 90 M30 50 L70 50 M40 30 L60 30 M35 40 L65 40" stroke="#008000" strokeWidth="2" />
+      <circle cx="50" cy="70" r="15" fill="#008000" />
+    </svg>
+  );
+};
+
+export default PlantIcon;
